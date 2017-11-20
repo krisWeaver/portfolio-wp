f040635837933cf05922eff9968e1348171d402a
Kris <kweaver@vervocity.io>
2017-11-20 01:20:32 -0600

    Initial Commit
    Starting commit to my new portfolio site using a child theme and ACF.

---
+++ wp-bootstrap-starter-child/home-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-md-12 col-lg-8">
+		<main id="main" class="site-main" role="main">
+
+			<h1>Hi Friend!</h1>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
