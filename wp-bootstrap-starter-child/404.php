a1c6f3de953f98c6290686f2ae108f5f5c55986c
Kris <kweaver@vervocity.io>
2017-11-24 01:19:12 -0600

    Added 404/ Changes
    CSS Changes
    Added 404 Template
    Added some CSS classes for 404

---
+++ wp-bootstrap-starter-child/404.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-md-12">
+		<main id="main" class="site-main" role="main">
+
+			<div class="home-header row">
+
+				<div class="col-md-12">
+					<div class="error-title">
+						Perhaps this is not the page you are looking for!
+					</div>
+                    <div class="error-text">
+                        If you reached this page by mistake please let me know and drop a line.
+                        <a href="https://krisweaverdesigns.com/contact-me/">Contact Me</a>
+                    </div>
+					<div class="error-icon">
+						<i class="fa fa-rebel" aria-hidden="true"></i>
+					</div>
+				</div>
+
+			</div>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
--- wp-bootstrap-starter-child/home-page.php
+++ wp-bootstrap-starter-child/home-page.php
@@ -9,14 +9,42 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-md-12 col-lg-8">
+	<section id="primary" class="content-area col-sm-12 col-md-12">
 		<main id="main" class="site-main" role="main">
 
-			<h1>Hi Friend!</h1>
+			<div class="home-header row">
+
+				<div class="col-md-4">
+					<div class="home-title">
+						About Me
+					</div>
+					<div class="home-icon">
+						<i class="fa fa-user" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="home-title">
+						My Portfolio
+					</div>
+					<div class="home-icon">
+						<i class="fa fa-paint-brush" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="home-title">
+						Contact Me
+					</div>
+					<div class="home-icon">
+						<i class="fa fa-comments-o" aria-hidden="true"></i>
+					</div>
+				</div>
+
+			</div>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
