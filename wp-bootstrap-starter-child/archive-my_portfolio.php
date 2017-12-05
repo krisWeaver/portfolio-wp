61ec40b988d7c34ce2764fdbea169f5db83c69d8
Kris <kweaver@vervocity.io>
2017-12-05 00:35:45 -0600

    Finished Styling Portfolio Archive Page
    Finished styling portfolio archive page and got buttons working. Also,
    added border to the right of an odd portfolio item to give it
    separation.

--- wp-bootstrap-starter-child/archive-my_portfolio.php
+++ wp-bootstrap-starter-child/archive-my_portfolio.php
@@ -16,41 +16,67 @@ get_header(); ?>
 				<h1>My Portfolio</h1>
 			</header><!-- .page-header -->
 
+
 			<?php
 				$args = array(
 						 'post_type' => 'my_portfolio',
 					'posts_per_page' => 6,
 				);
 
+				$portfolioNumber = 0;
+
 				$the_query = new WP_Query( $args );
 				if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+
+				$portfolioNumber = $portfolioNumber + 1;
 			?>
 
-			<?php
+					<?php
+						if ( $portfolioNumber > 1 ) {
+							echo "<div class='col-md-6'>";					
+						} else {
+							echo "<div class='row portfolio-row'>";
+							echo "<div class='col-md-6 separator'>";
+						}
+					?>
 
-				$image = get_field('logo');
+					<?php
 
-					if( !empty($image) ): ?>
+						$image = get_field('logo');
+
+						if( !empty($image) ):
+					?>
 
 						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
-					<?php endif; ?>
+						<?php endif; ?>
 
-				<div class="row">
+						<div class="row portfolio-buttons-row">
 
-					<div class="col-md-6 portfolio-button">
-						<button type="button" class="btn btn-primary">Visit Site</button>
-					</div>
+							<div class="col-md-6 portfolio-button">
+								<a href="<?php echo get_field('work_link');?>" target="_blank" class="btn btn-primary">View Website</a>
+							</div>
 
-					<div class="col-md-6 portfolio-button">
-						<button type="button" class="btn btn-primary">View Project</button>
-					</div>
+							<div class="col-md-6 portfolio-button">
+								<a href="<?php the_permalink(); ?>" class="btn btn-primary">View Project</a>
+							</div>
 
-				</div>
+						</div> <!-- End Button Rows -->
 
-			<?php endwhile; else: ?>
-					<p><?php _e('Sorry, no content available'); ?></p>
+					</div> <!-- End Col Md 6-->
+
+					<?php
+						if ( $portfolioNumber > 1 ) {
+							echo " </div> <!-- End Row -->";
+							$portfolioNumber = 0;
+						}
+					?>
+
+				<?php endwhile; else: ?>
+						<p><?php _e('Sorry, no content available'); ?></p>
 			<?php endif; ?>
+
+
 
 
 		</main><!-- #main -->
